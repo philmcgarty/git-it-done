ce8c9dbb41f1fbb127a66ebc5c1a03e20619dc53
Phil McGarty <philmcgarty@hotmail.com>
2022-03-25 22:55:43 -0400

    added language search functionality

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,24 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
+
+
+
+var getFeaturedRepos = function(language){
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+    fetch(apiUrl).then(function(response){
+        if (response.ok) {
+            response.json().then(function(data){
+              displayRepos(data.items, language);  
+            });            
+        } else {
+            alert("Error: Github User Not Found");
+        }
+    });
+};
+
 
 var displayRepos = function(repos, searchTerm){
     console.log(repos);
@@ -86,5 +104,20 @@ var formSubmitHandler = function(event){
     }
     // console.log(event);
 };
+
+var buttonClickHandler = function(event){
+    var language = event.target.getAttribute("data-language")
+    console.log(language);
+    
+    if (language){
+        getFeaturedRepos(language);
+        // clear old content
+        repoContainerEl.textContent = "";
+    }
+}
+
+
+
+languageButtonsEl.addEventListener("click", buttonClickHandler);
 
 userFormEl.addEventListener("submit", formSubmitHandler);
